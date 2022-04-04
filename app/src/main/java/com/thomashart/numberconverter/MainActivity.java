7f6ebea0dedd636e5e82a0747c841ff5654e1f8c
Thomas Hart <thomas.hart@uwaterloo.ca>
2022-04-03 23:49:00 -0700

    Basic layout

--- app/src/main/java/com/thomashart/numberconverter/MainActivity.java
+++ app/src/main/java/com/thomashart/numberconverter/MainActivity.java
@@ -2,12 +2,26 @@ package com.thomashart.numberconverter;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class MainActivity extends Activity {
+
+    private Spinner spinner1, spinner2;
+    private ArrayAdapter<CharSequence> arrayAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        spinner1 = findViewById(R.id.spinner1);
+        spinner2 = findViewById(R.id.spinner2);
+
+        arrayAdapter = ArrayAdapter.createFromResource(this, R.array.spinner_items, android.R.layout.simple_spinner_item);
+        arrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        spinner1.setAdapter(arrayAdapter);
+        spinner2.setAdapter(arrayAdapter);
     }
 }
